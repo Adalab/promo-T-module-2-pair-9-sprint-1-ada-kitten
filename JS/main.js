a7b1890e78fec8dff991bb5a0bf797602314fa40
SuelenGalhardo <suelenranucci@gmail.com>
2023-05-19 11:23:18 +0200

    variablescat

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,25 @@
 
 const ListSection = document.querySelector('.js-list');
 
-const kittentOne = `<li class="card">
+const kittentOne = 
+const kittenImagen = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName = 'Anastacio';
+const kittenDesc = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenRace = 'Siamés';
+
+const kittenTwo =
+const kittenImagen = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenName = 'Fiona';
+const kittenDesc = 'Produce fascinación y curiosidad.' + ' Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.'; 
+const kittenTwo = 'Sphynx';
+
+const KittenThree =
+const kittenImagen = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenName = 'Cielo';
+const kittenDesc = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha' + 'convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta. '
+const kittenRace = 'Maine Coon'; 
+
+/*`<li class="card">
 <article>
   <img class="card_img" src="https://dev.adalab.es/gato-siames.webp" alt="siames-cat" />
   <h3 class="card_title">Anastacio</h3>
@@ -37,7 +55,7 @@ const KittenThree =  `<li class="card">
   convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas
   resultan largas y en punta.
 </p>
-</li>`;
+</li>`;*/
 
 ListSection.innerHTML = ListSection.innerHTML + kittentOne; 
 ListSection.innerHTML = ListSection.innerHTML + kittenTwo;
