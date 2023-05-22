52cb07d22cef77f02e20047161644c470beb7eb5
SuelenGalhardo <suelenranucci@gmail.com>
2023-05-22 11:07:20 +0200

    variables

--- JS/main.js
+++ JS/main.js
@@ -2,59 +2,56 @@
 
 const ListSection = document.querySelector('.js-list');
 
-const kittentOne = 
+const kittenImagenOne = 'https://dev.adalab.es/gato-siames.webp';
+const kittenNameOne = 'Anastacio';
+const kittenDescOne = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenRaceOne = 'Siamés';
+
+const kittenOne = 
 `<li class="card">
 <article>
-  <img class="card_img" src="https://dev.adalab.es/gato-siames.webp" alt="siames-cat" />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
+  <img class="card_img" src="${kittenImagenOne}"/>
+  <h3 class="card_title">${kittenNameOne}</h3>
+  <h4 class="card_race">${kittenRaceOne}</h4>
   <p class="card_description">
-    Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso,
-    pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy
-    posiblemente.
+    ${kittenDescOne}
   </p>
 </article>
 </li>`;
-const kittenImagen = 'https://dev.adalab.es/gato-siames.webp';
-const kittenName = 'Anastacio';
-const kittenDesc = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-const kittenRace = 'Siamés';
+
+const kittenImagenTwo = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenNameTwo = 'Fiona';
+const kittenDescTwo = 'Produce fascinación y curiosidad.' + ' Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.'; 
+const kittenRaceTwo = 'Sphynx';
 
 const kittenTwo = `<li class="card">
-<img class="card_img" src="https://dev.adalab.es/sphynx-gato.webp" alt="sphynx-cat" />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">Sphynx</h4>
+<img class="card_img" src="${kittenImagenTwo}" alt="sphynx-cat" />
+<h3 class="card_title">${kittenNameTwo}</h3>
+<h4 class="card_race">${kittenRaceTwo}</h4>
 <p class="card_description">
-  Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de
-  alienígena han llegado a definir a esta raza gatuna que se caracteriza por la
-  «ausencia» de pelo.
+ ${kittenDescTwo}
 </p>
 </li>`;
-const kittenImagen = 'https://dev.adalab.es/sphynx-gato.webp';
-const kittenName = 'Fiona';
-const kittenDesc = 'Produce fascinación y curiosidad.' + ' Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.'; 
-const kittenTwo = 'Sphynx';
 
+const kittenImagenThreee = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenNameThreee = 'Cielo';
+const kittenDescThreee = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha' + 'convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const kittenRaceThreee = 'Maine Coon';
 const KittenThree =  `<li class="card">
 <img
   class="card_img"
-  src="https://dev.adalab.es/maine-coon-cat.webp"
+  src="${kittenImagenThreee}"
   alt="maine-coon-cat"
 />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">Maine Coon</h4>
+<h3 class="card_title">${kittenNameThreee}</h3>
+<h4 class="card_race">${kittenRaceThreee}</h4>
 <p class="card_description">
-  Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha
-  convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas
-  resultan largas y en punta.
+  ${kittenDescThreee}
 </p>
 </li>`;
-const kittenImagen = 'https://dev.adalab.es/maine-coon-cat.webp';
-const kittenName = 'Cielo';
-const kittenDesc = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha' + 'convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
-const kittenRace = 'Maine Coon';
 
-ListSection.innerHTML = ListSection.innerHTML + kittentOne; 
+
+ListSection.innerHTML = ListSection.innerHTML + kittenOne; 
 ListSection.innerHTML = ListSection.innerHTML + kittenTwo;
 ListSection.innerHTML = ListSection.innerHTML + KittenThree; 
 
